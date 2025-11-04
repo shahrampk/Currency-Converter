61a42804241bd577b327dbe44b61f3eee8c04ea1
Muhammad Shahram <mshahram574@gmail.com>
2025-11-04 22:34:17 +0500

    Fix import casing for useCurrencyInfo hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { InputBox } from "./components";
-import UseCurrencyInfo from "./hooks/useCurrencyinfo";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 const App = () => {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("pkr");
   const [convertedAmount, setConvertedAmount] = useState(0);
-  const currencyInfo = UseCurrencyInfo(from);
+  const currencyInfo = useCurrencyInfo(from);
 
   const options = Object.keys(currencyInfo);
   const swap = () => {
